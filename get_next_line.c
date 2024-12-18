3a37d9fd6d2dd7848fd84a22b7cd9363a453466a
Armand Mosnier <armosnie@MADE-f0Cr9s4.clusters.42paris.fr>
2024-12-18 18:01:01 +0100

    gnl update

--- get_next_line.c
+++ get_next_line.c
@@ -6,36 +6,11 @@
 /*   By: armosnie <armosnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 14:50:20 by armosnie          #+#    #+#             */
-/*   Updated: 2024/12/18 17:26:28 by armosnie         ###   ########.fr       */
+/*   Updated: 2024/12/18 17:42:07 by armosnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*get_next_line(int fd)
-{
-	char		buf[BUFFER_SIZE + 1];
-	int			size_read;
-	static char	*str;
-	char		*line;
-
-	size_read = BUFFER_SIZE;
-	if (str == NULL)
-		str = ft_strndup("", '\n');
-	while (size_read == BUFFER_SIZE && ft_strchr(str, '\n') == 0)
-	{
-		size_read = read(fd, buf, BUFFER_SIZE);
-		if (size_read == -1)
-		{
-			return (NULL);
-		}
-		buf[size_read] = '\0';
-		str = ft_strjoin(str, buf);
-	}
-	line = ft_strndup(str, '\n');
-	str = ft_clean_str(str);
-	return (line);
-}
 
 char	*ft_clean_str(char *str)
 {
@@ -56,6 +31,34 @@ char	*ft_clean_str(char *str)
 		str = ft_strndup(str + i, '\n');
 	}
 	return (str);
+}
+
+char	*return_read(char buf[BUFFER_SIZE]);
+
+char	*get_next_line(int fd)
+{
+	char		buf[BUFFER_SIZE + 1];
+	int			size_read;
+	static char	*str;
+	char		*line;
+
+	size_read = BUFFER_SIZE;
+	if (str == NULL)
+		str = ft_strndup("", '\n');
+	while (size_read == BUFFER_SIZE && ft_strchr(str, '\n') == 0)
+	{
+		size_read = read(fd, buf, BUFFER_SIZE);
+		if (size_read == -1)
+		{
+			free(str);
+			return (NULL);
+		}
+		buf[size_read] = '\0';
+		str = ft_strjoin(str, buf);
+	}
+	line = ft_strndup(str, '\n');
+	str = ft_clean_str(str);
+	return (line);
 }
 
 int	main(void)
